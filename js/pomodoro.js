21663fa495ff5c87301a6665bcd8160c1baac735
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-16 18:12:28 -0500

    update timer displays when slider is moved

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -3,4 +3,12 @@ function showTimeValue(pomTime) {
 	console.log(pomTime);
 	var breakTime = 60 - pomTime;
 	console.log(breakTime);
+	updateTimeDisplays(pomTime, breakTime);
+}
+
+function updateTimeDisplays(sessionTime, breakTime) {
+	sTimer = document.getElementById("sessionTimeAmount");
+	sTimer.innerHTML = sessionTime + ":00";
+	bTimer = document.getElementById("breakTimeAmount");
+	bTimer.innerHTML = breakTime + ":00";
 }
