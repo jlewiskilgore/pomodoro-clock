711e7b8be7d362791c89871a982ef5fa217d878d
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-17 20:23:05 -0500

    Set up countdown time to update each second

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,3 +1,6 @@
+$( document ).ready(function() {
+    updateTimeDisplays(30,30); //initial values of slider
+});
 
 function showTimeValue(pomTime) {
 	console.log(pomTime);
@@ -24,6 +27,8 @@ function startTimer() {
 	//Show Timer Div
 	pomTimer = document.getElementById("pomodoroTimer");
 	pomTimer.style.display = "inline";
+	//Start Countdown
+	countdownTimer(startingTime);
 }
 
 function endTimer() {
@@ -33,4 +38,34 @@ function endTimer() {
 	//Show Setup Div
 	var pomTimerSetup = document.getElementById("pomodoroTimerSetup");
 	pomTimerSetup.style.display = "inline";
+}
+
+function countdownTimer(startTime) {
+	console.log("starting countdown");
+	startTime = startTime.split(":");
+	startTime = startTime[0];
+	console.log(startTime);
+	var startTimeSec = startTime * 60; //convert to seconds
+
+	var timerDisplay = document.getElementById("mainTimerDisplay");
+	var timeDisplayStr;
+
+	setInterval(function() {
+		if(startTimeSec > 0){
+			startTimeSec--;
+			timeDisplayStr = formatClockDisplay(startTimeSec);
+			timerDisplay.innerHTML = timeDisplayStr;
+		}
+		else
+			return;
+	}, 1000);
+}
+
+function formatClockDisplay(timeSeconds) {
+	var minutes = Math.floor(timeSeconds / 60);
+	var seconds = timeSeconds % 60;
+	if(seconds < 10) {
+		seconds = "0"+seconds;
+	}
+	return minutes + ":" + seconds;
 }
